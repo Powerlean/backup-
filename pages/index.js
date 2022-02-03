8e4231123fd939931fd02dbd527acce2eb5bcb78
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-02-03 11:49:07 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ import sunn from '/images/sunn.png';
 const Home = () => (
   <Container>
 \n\n\n\n\n\n
-<Image alt="sunn logo" src={sunn} width={1446} height={813} />
+<Image alt="sunn logo" src={sunn} width={1157} height={650} />
     <Box mt={10} mb={6} p={6}>
       <Badge mt={4} mb={5}>
         關於
