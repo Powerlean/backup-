bc4c8622d3c688cf83b22a57f99ac7cbb85cc3e1
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-02-03 12:13:53 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,6 @@ import sunn from '/images/sunn.png';
 
 const Home = () => (
   <Container>
-
-<Image alt="sunn logo" src={sunn} width={868} height={488} />
     <Box mt={10} mb={6} p={6}>
       <Badge mt={4} mb={5}>
         關於
