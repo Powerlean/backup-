0a70da28a6ee976bfcf55379dcd4cb58899675a3
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-02-05 13:54:56 +0800

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,8 @@ import { Box } from "@chakra-ui/react";
 const Footer = () => {
   return (
     <Box align="center" fontSize="sm">
-       &copy;{new Date().getFullYear()} SUNN，由「Willie」強力驅動 
+       &copy;{new Date().getFullYear()} SUNN，由「Willie」強力驅動
+ 站點整備中 (-_-;)・・・ 
     </Box>
   );
 };
