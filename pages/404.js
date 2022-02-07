81e41f6185b6adf5929efa1408171eee96846a5f
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-02-07 10:04:51 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -7,12 +7,14 @@ import {
   Divider,
   Button,
 } from "@chakra-ui/react";
+import Image from 'next/image';
+import icon404 from '/images/404.png';
 
 const NotFound = () => {
   return (
     <Container>
       <Box align="center">
-
+      <Image alt="icon404" src={icon404} width={1500} height={1500} />
         <Heading as="h1" mt={9}>
           404
         </Heading>
