a5e2689bf6f7e07f484d9aa4a23c56846e03e21d
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-02-07 23:52:02 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -21,7 +21,7 @@ const NotFound = () => {
 
         <Text mt={2}> 嚄...好像迷路囉？ </Text>
         <Divider my={6} />
-<Image alt="notfound" src="/images/bak.png" width={360} height={350} />
+<Image alt="notfound" src="/images/bak.png" width={360} height={340} />
       </Box>
       <Box my={6} align="center">
         <NextLink href="/">
