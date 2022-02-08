7a960154a9fc20bcae4dbffe7792a34a527a5e93
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-02-08 23:41:04 +0800

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -4,9 +4,7 @@ const Footer = () => {
   return (
     <Box align="center" fontSize="sm">
 
-<p>
-            聯絡我們：contact@liberas.studio
-          </p>
+
        &copy;{new Date().getFullYear()} SUNN，由「Willie」強力驅動
     </Box>
   );
