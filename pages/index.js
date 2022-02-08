94442009f16a5a20ebd4711d5422cb614c2572a2
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-02-08 17:30:44 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ const Home = () => (
   <Container>
     <Box mt={10} mb={6} p={6}>
       <Stat mt={6} mb={5}>
-<Image alt="back logo" src="https://imgtu.com/i/H1H7dO" width={1955} height={1080} />
+<Image alt="back logo" src="https://s4.ax1x.com/2022/02/08/H1H7dO.png" width={1955} height={1080} />
         <StatLabel>我是</StatLabel>
         <StatNumber>SUNN</StatNumber>
          
