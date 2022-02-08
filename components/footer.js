70964c9d325848f9b5a441d900d7a376a14d3c0f
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-02-09 00:34:35 +0800

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react";
 const Footer = () => {
   return (
     <Box align="center" fontSize="sm">
-
+<Image src='/images/footer.png' alt="footer logo" rounded="md" boxSize='30px' objectFit='cover' my={4} />
 
        &copy;{new Date().getFullYear()} SUNN，由「Willie」強力驅動
     </Box>
