74b53dc9e4248ea8adcaa511ae51b65b6824cdf0
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-02-08 22:24:17 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -15,7 +15,8 @@ const NotFound = () => {
       <Box align="center" mt={20}>
         <Image alt="notfound" src="/images/bak.png" width={360} height={330} />
        <Heading fontFamily="sans-serif" fontSize="2xl" fontWeight="bold">嚄...好像迷路囉？</Heading>
-      </Box>
+       <Heading fontFamily="sans-serif" fontSize="1xl">您所搜尋的資訊不存在亦或無訪問權限。</Heading>  
+</Box>
       <Box my={6} align="center">
         <NextLink href="/">
           <Button colorScheme="gray">返回主頁</Button>
