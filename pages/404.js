843bf813de829096d756a5b0dda906fa809762e3
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-02-08 22:17:30 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -14,7 +14,7 @@ const NotFound = () => {
     <Container>
       <Box align="center" mt={20}>
         <Image alt="notfound" src="/images/bak.png" width={360} height={330} />
-       <Heading fontFamily="sans-serif" fontSize="2xl" fontWeight="bold">嚄...好像迷路囉？</Heading>
+       <Heading fontFamily="sans-serif" fontSize="1xl" fontWeight="bold">嚄...好像迷路囉？</Heading>
       </Box>
       <Box my={6} align="center">
         <NextLink href="/">
