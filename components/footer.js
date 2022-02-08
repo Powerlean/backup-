3c617a7ce14de36185e97beb04223deb444c8aea
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-02-09 01:28:39 +0800

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,7 @@ import {
 const Footer = () => {
   return (
      <Box align="center" fontSize="sm">
-<Image src='/images/trademark.png' alt="footer logo" rounded="none" boxSize='100px' objectFit='none' my={2} />
+<Image src='/images/trademark.png' alt="footer logo" rounded="none" boxSize='400px' objectFit='none' my={2} />
        &copy;{new Date().getFullYear()} SUNN，由「Willie」強力驅動
     </Box>
   );
