de38fba89920c990a4cbdd790f9e99b1ecd3cb8a
Powerlean <52900841+Powerlean@users.noreply.github.com>
2022-07-14 21:12:35 +0800

    Update logo.js

--- components/logo.js
+++ components/logo.js
@@ -4,7 +4,7 @@ import styled from "@emotion/styled";
 
 const Logobox = styled.span`
   font-weight: bold;
-  font-size: 30px;
+  font-size: 40px;
   display: inline-flex;
   align-items: center;
   padding: 10px;
